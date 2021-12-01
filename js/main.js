12f4aa871a76c7295a80d27355e1943e58d13daa
Jerônimo <jeronimoaa@al.insper.edu.br>
2021-12-01 17:53:23 -0300

    Aperfeiçoamento do efeito de clique

--- js/main.js
+++ js/main.js
@@ -1,30 +1,88 @@
 
-const elementos_li = document.getElementsByTagName("li");
+const ancoras = document.getElementsByTagName("a");
 
-function pintarCliqueLi(evento) {
+function liDown(evento) {
 
 	const elemento = evento.target;
-	const ancora = elemento.getElementsByTagName("a")[0]
+	const ancora = elemento.getElementsByTagName("a")[0];
 
+	ancora.style.color = "#CF5E4A";
 	elemento.style.color = "#CF5E4A";
 	elemento.style.borderColor = "#CF5E4A";
-	ancora.style.color = "#CF5E4A";
 }
 
-function despintarCliqueLi(evento) {
+function liUp(evento) {
 
 	const elemento = evento.target;
-	const ancora = elemento.getElementsByTagName("a")[0]
+	const ancora = elemento.getElementsByTagName("a")[0];
 
-	elemento.style.color = "#FFFFFF";
+	ancora.style.color = "white";
+	elemento.style.color = "#white";
 	elemento.style.borderColor = "#428C99";
-	ancora.style.color = "#FFFFFF";
 
-	window.location.href = ancora.href;
+	document.location = ancora.href;
 }
 
-for (var i = 0; i < elementos_li.length; i++) {
-	elementos_li[i].addEventListener("mousedown", pintarCliqueLi, false);
-	elementos_li[i].addEventListener("mouseup", despintarCliqueLi, false);
+function aDown(evento) {
+
+	const ancora = evento.target;
+	const elemento = ancora.parentElement;
+
+	ancora.style.color = "#CF5E4A";
+	elemento.style.color = "#CF5E4A";
+	elemento.style.borderColor = "#CF5E4A";
 }
+
+function aUp(evento) {
+
+	const ancora = evento.target;
+	const elemento = ancora.parentElement;
+
+	ancora.style.color = "white";
+	elemento.style.color = "#white";
+	elemento.style.borderColor = "#428C99";
+}
+
+function aDownImg(evento) {
+
+	const imagem = evento.target;
+	var arquivo_imagem = imagem.src;
+	arquivo_imagem = arquivo_imagem.slice(0, -4);
+	arquivo_imagem += "_clicado.jpg";
+	imagem.src = arquivo_imagem;
+}
+
+function aUpImg(evento) {
+
+	const imagem = evento.target;
+	var arquivo_imagem = imagem.src;
+	arquivo_imagem = arquivo_imagem.slice(0, -12);
+	arquivo_imagem += ".jpg";
+	imagem.src = arquivo_imagem;
+}
+
+for (var i = 0; i < ancoras.length; i++) {
+
+	const ancora = ancoras[i];
+	const elemento = ancora.parentElement;
+
+	if (elemento.nodeName == "HEADER") {
+		ancora.addEventListener("mousedown", aDownImg, false);
+		ancora.addEventListener("mouseup", aUpImg, false);
+
+	} else {
+
+		ancora.addEventListener("mousedown", aDown, false);
+		ancora.addEventListener("mouseup", aUp, false);
+
+		if (elemento.nodeName == "LI") {
+			elemento.addEventListener("mousedown", liDown, false);
+			elemento.addEventListener("mouseup", liUp, false);
+		}
+	}
+}
+
+
+
+
 
